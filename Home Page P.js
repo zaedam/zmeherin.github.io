418dfb9a92e1af4fedccba3514fb0c173ef5c147
Zaeda <zmeherin@gmail.com>
2019-07-10 15:31:57 -0400

    fixing scroll 2

--- Home Page P.js
+++ Home Page P.js
@@ -1,17 +1,16 @@
-// $(document).ready(function () {
-	function scrollToPgTwo() {
-		$('html,body').animate({ scrollTop: $("#PgTwo").offset().top}, 700) 
-		event.preventDefault(); // Prevents page from flashing before scrolling due to race condition
-		// e.stopPropagation;
-	}
-	function scrollToPgThree() {
-		$('html,body').animate({ scrollTop: $("#PgThree").offset().top}, 1400);
-		event.preventDefault();
-		// e.stopPropagation;
-	}
-	function scrollToPgFour() {
-		$('html,body').animate({ scrollTop: $("#PgFour").offset().top}, 2100);
-		event.preventDefault();
-		// e.stopPropagation;
-	}
-// });
+function scrollToPgTwo() {
+	$('html,body').animate({
+		scrollTop: $("#PgTwo").offset().top}, 700) 
+	event.preventDefault(); // Prevents page from flashing before scrolling due to race condition
+	// e.stopPropagation; //Home Page P.js:5 Uncaught ReferenceError: e is not defined
+}
+function scrollToPgThree() {
+	$('html,body').animate({ scrollTop: $("#PgThree").offset().top}, 1400);
+	event.preventDefault();
+	// e.stopPropagation;
+}
+function scrollToPgFour() {
+	$('html,body').animate({ scrollTop: $("#PgFour").offset().top}, 2100);
+	event.preventDefault();
+	// e.stopPropagation;
+}
