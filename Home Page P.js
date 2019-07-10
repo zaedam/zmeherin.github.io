eaf7b7581cee8d13e036a5503e26120c6d8718f3
Zaeda <zmeherin@gmail.com>
2019-07-10 16:57:17 -0400

    fixing mobile responsiveness

--- Home Page P.js
+++ Home Page P.js
@@ -2,7 +2,7 @@ function scrollToPgTwo() {
 	$('html,body').animate({
 		scrollTop: $("#PgTwo").offset().top}, 700) 
 	event.preventDefault(); // Prevents page from flashing before scrolling due to race condition
-	// e.stopPropagation; //Home Page P.js:5 Uncaught ReferenceError: e is not defined
+	// e.stopPropagation; //Uncaught ReferenceError: e is not defined
 }
 function scrollToPgThree() {
 	$('html,body').animate({ scrollTop: $("#PgThree").offset().top}, 1400);
